24d25adaf6479eb0351240c39c3a53933cd9c370
Terrance Smith <terrance.smithy@gmail.com>
2015-07-06 20:18:48 -0500

    Create the Data Model and  Database Context

---
+++ src/Contoso-University/DAL/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contoso_University.DAL
+{
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using Models;
+
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext()
+            : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ src/Contoso-University/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿namespace Contoso_University.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
